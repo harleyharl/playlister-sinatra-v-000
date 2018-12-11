081b9915d6397128e0288eff73c3df6e13128f59
Harley Rayner <harleymteden@gmail.com>
2018-12-11 21:08:33 +0000

    Automatically backed up by Learn

--- app/controllers/genres_controller.rb
+++ app/controllers/genres_controller.rb
@@ -5,11 +5,14 @@ class GenresController < ApplicationController
     erb :'genres/index'
   end
 
+<<<<<<< HEAD
   get '/genres/:slug' do
     slug = params[:slug]
     @genre = Genre.find_by_slug(slug)
     erb :'genres/show'
   end
 
+=======
+>>>>>>> b5d095650124027f8d19eed38531070289971ef5
 
 end
